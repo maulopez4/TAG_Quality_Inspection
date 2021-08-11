7c58680d6a967f087799d9fc996644e29e11e135
Mauricio Lopez <maulopez4@gmail.com>
2021-08-11 15:48:09 -0700

    Corrected Login Form elements

--- LoginForm.vb
+++ LoginForm.vb
@@ -5,10 +5,10 @@ Public Class LoginForm
     Friend Shared login_user As String
     Private ReadOnly connection_string As String = ConfigurationManager.ConnectionStrings("tag_quality").ConnectionString
     Private ReadOnly connection As New MySqlConnection(connection_string)
-    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Login_OKButton.Click
         Dim command As New MySqlCommand("SELECT `users_username`, `users_password`, `users_role`, `users_status` FROM `users` WHERE `users_username` = @username AND `users_password` = @password", connection)
-        command.Parameters.Add("@username", MySqlDbType.VarChar).Value = UsernameTextBox.Text
-        command.Parameters.Add("@password", MySqlDbType.VarChar).Value = PasswordTextBox.Text
+        command.Parameters.Add("@username", MySqlDbType.VarChar).Value = Login_UsernameTextBox.Text
+        command.Parameters.Add("@password", MySqlDbType.VarChar).Value = Login_PasswordTextBox.Text
         Dim adapter As New MySqlDataAdapter(command)
         Dim table As New DataTable()
         adapter.Fill(table)
@@ -25,14 +25,14 @@ Public Class LoginForm
             Me.Hide()
         End If
     End Sub
-    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Login_CancelButton.Click
         Me.Close()
     End Sub
-    Private Sub ShowPasswordCheckBox_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ShowPasswordCheckBox.CheckedChanged
-        If PasswordTextBox.UseSystemPasswordChar = True Then
-            PasswordTextBox.UseSystemPasswordChar = False
+    Private Sub ShowPasswordCheckBox_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Login_ShowPasswordCheckBox.CheckedChanged
+        If Login_PasswordTextBox.UseSystemPasswordChar = True Then
+            Login_PasswordTextBox.UseSystemPasswordChar = False
         Else
-            PasswordTextBox.UseSystemPasswordChar = True
+            Login_PasswordTextBox.UseSystemPasswordChar = True
         End If
     End Sub
 End Class
